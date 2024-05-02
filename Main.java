cd0ed0a1a6fd1bc85f49fa9324c7d89a16357cd7
Ardra-14 <144412428+Ardra-14@users.noreply.github.com>
2024-05-02 10:30:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = s.nextInt();
+
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        int i;
+        int j;
+
+        for (i = 0, j = size - 1; i <= j; ) {
+            if (arr[i] != 0) {
+                i++;
+            } else if (arr[j] == 0) {
+                j--;
+            } else {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        System.out.println("Array after moving zero to end: " + Arrays.toString(arr));
+
+    }
+}
